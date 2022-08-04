d06b12f90f43638d81af6ebe4da150e95b59fd3a
Marc D Wagner <marc.wagner@yahoo.com>
2022-08-04 16:53:00 -0700

    image added

--- script.js
+++ script.js
@@ -6,13 +6,13 @@
 // document.write("Hello, " + response + ", welcome to my page.")
 // console.log(response);
 
-let likeBunnies = "yes";
+// let likeBunnies = "yes";
 
-let response; 
-while (response !== likeBunnies){
-    response = (prompt( "Do you like bunnies?" )).toLowerCase();
-    if (response !== likeBunnies) {
-        alert( "Are you sure?")
-    }
-}
+// let response; 
+// while (response !== likeBunnies){
+//     response = (prompt( "Do you like bunnies?" )).toLowerCase();
+//     if (response !== likeBunnies) {
+//         alert( "Are you sure?")
+//     }
+// }
 
