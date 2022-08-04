e391993bb3d33046a7c7c4b5dceef619fd232ab0
Marc D Wagner <marc.wagner@yahoo.com>
2022-08-04 16:27:00 -0700

    js loop

--- script.js
+++ script.js
@@ -2,9 +2,17 @@
 
 
 
-let response = prompt("Hi, what is your name?");
-document.write("Hello, " + response + ", welcome to my page.")
-console.log(response);
+// let response = prompt("Hi, what is your name?");
+// document.write("Hello, " + response + ", welcome to my page.")
+// console.log(response);
 
+let likeBunnies = "yes";
 
+let response; 
+while (response !== likeBunnies){
+    response = (prompt( "Do you like bunnies?" )).toLowerCase();
+    if (response !== likeBunnies) {
+        alert( "Are you sure?")
+    }
+}
 
