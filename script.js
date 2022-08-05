a4e9f109f39aa68252e7acc17a080e2076638e2a
Marc D Wagner <marc.wagner@yahoo.com>
2022-08-05 15:30:30 -0700

    js redo

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 
 
-
-
-// let response = prompt("Hi, what is your name?");
-// document.write("Hello, " + response + ", welcome to my page.")
-// console.log(response);
-
-// function ask(){
-    let response = prompt("How many photos would you like to see? 1 to 3");{
-    for (let i = 1; i <= response; i= i + 1){
-        document.write('<img src="photos/Easter.jpg"/>');
-        console.log(response);
-    }
+function getName(){
+    let userName = prompt("What is your name?");
+    if (userName == "" || userName == null || userName == undefined) {
+        alert("Nice try");
+        getName();
+    } else {
+        document.write("Hey " + userName + ", welcome to the bunny page!");
+        }
 }
+getName()
 
-// let likeBunnies = "yes";
-
-// let response; 
-// while (response !== likeBunnies){
-//     response = (prompt( "Do you like bunnies?" )).toLowerCase();
-//     if (response !== likeBunnies) {
-//         alert( "Are you sure?")
+// function askLikeBunnies(){
+//     let response = prompt("Do you like bunnies?");
+//     if (response == "yes"){
+//         alert("You're in the right place!");
 //     }
+//     else if (response == "no"){
+//         alert("Are you sure? Try again.");
+//     }
+//     else if {
+//         alert("Please answer yes or no.");
+//     }
+//     // response = (prompt("Do you like bunnies?")) .toLowerCase();
 // }
 
-// let bunniesToShow = 5;
-// for (let i = 1, i <= bunniesToShow; i++ ){
-// console.log( photos/sploot..JPG );
-// }
+// askLikeBunnies()
+
 
